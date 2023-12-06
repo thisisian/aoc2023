67281954688b4d3019f11d1fa110e97a53a36abd
Ian <imianwinter@gmail.com>
2023-12-05 22:28:33 -0800

    day 5

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![feature(ascii_char)]
+#![feature(iter_array_chunks)]
 pub mod day1;
 pub mod day2;
 pub mod day3;
+pub mod day4;
+pub mod day5;
 
 fn main() {
-    day3::solve();
+    day5::solve();
 }
-
-
